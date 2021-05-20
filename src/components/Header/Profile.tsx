9812ca6dfd757397b78dbbe665e9a6744e4a676d
Wolgrand Neto <wolgrand@live.com>
2021-05-20 14:12:48 -0300

    Fix Profile Information

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,20 +1,24 @@
 import {Flex, Text, Box, Avatar} from '@chakra-ui/react'
+import { useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
 
 interface ProfileProps {
   showProfileData?: boolean
 }
 
 export default function Profile({showProfileData = true}:ProfileProps) {
+  const { user } = useContext(AuthContext)
+
   return (
     <Flex align="center">
       { showProfileData && (
         <Box mr="4" textAlign="right">
-          <Text>Wolgrand Neto</Text>
-          <Text color="gray.300" fontSize="small">wolgrand@email.com</Text>
+          <Text>{user.name}</Text>
+          <Text color="gray.300" fontSize="small">{user.email}</Text>
         </Box>
       )}
 
-      <Avatar size="md" name="Wolgrand" src="https://github.com/wolgrand.png"/>
+      <Avatar size="md" name={user?.name} src={user?.image_url}/> 
     </Flex>
   )
 }
--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -8,11 +8,7 @@ export default function SidebarNav(){
     <Stack spacing="12" align="flex-start">
       <NavSection title="GERAL">
         <NavLink icon={RiDashboardLine} href="/dashboard"> Dashboard </NavLink>
-        <NavLink icon={RiContactsLine} href="/users"> Usuários </NavLink>
-      </NavSection>
-      <NavSection title="AUTOMAÇÃO">
-        <NavLink icon={RiInputMethodLine} href="/forms"> Formulário </NavLink>
-        <NavLink icon={RiGitMergeLine} href="/automation"> Automação </NavLink>
+        {/* <NavLink icon={RiContactsLine} href="/users"> Usuários </NavLink> */}
       </NavSection>
     </Stack>
   )
